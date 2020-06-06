dac1281389a8b9af7fdf07224e9d8f5ea7ae0065
Zyro9922 <alihasan9922@gmail.com>
2020-06-06 14:36:54 +0530

    Test equatorial_coord_default_right_ascension_constructor

--- test/coordinate/equatorial_coord.cpp
+++ test/coordinate/equatorial_coord.cpp
@@ -18,28 +18,61 @@ namespace bu = boost::units;
 
 BOOST_AUTO_TEST_SUITE(angle)
 
-    BOOST_AUTO_TEST_CASE(right_ascension) {
-        //Create object of Right Ascension
-        RightAscension<double, quantity<bud::plane_angle>>
-                ra(25.0 * bud::degrees);
+BOOST_AUTO_TEST_CASE(right_ascension)
+{
+    //Create object of Right Ascension
+    RightAscension<double, quantity<bud::plane_angle>>
+            ra(25.0 * bud::degrees);
 
-        //Check value
-        BOOST_CHECK_CLOSE(ra.get_angle().value(), 25.0, 0.001);
+    //Check value
+    BOOST_CHECK_CLOSE(ra.get_angle().value(), 25.0, 0.001);
 
-        //Quantity stored as expected?
-        BOOST_TEST((std::is_same<decltype(ra.get_angle()), quantity<bud::plane_angle>>::value));
-    }
+    //Quantity stored as expected?
+    BOOST_TEST((std::is_same<decltype(ra.get_angle()), quantity<bud::plane_angle>>::value));
+}
 
-    BOOST_AUTO_TEST_CASE(hour_angle) {
-        //Create object of Hour Angle
-        HourAngle<double, quantity<bud::plane_angle>>
-                ha(25.0 * bud::degrees);
+BOOST_AUTO_TEST_CASE(hour_angle)
+{
+    //Create object of Hour Angle
+    HourAngle<double, quantity<bud::plane_angle>>
+            ha(25.0 * bud::degrees);
 
-        //Check value
-        BOOST_CHECK_CLOSE(ha.get_angle().value(), 25.0, 0.001);
+    //Check value
+    BOOST_CHECK_CLOSE(ha.get_angle().value(), 25.0, 0.001);
 
-        //Quantity stored as expected?
-        BOOST_TEST((std::is_same<decltype(ha.get_angle()), quantity<bud::plane_angle>>::value));
-    }
+    //Quantity stored as expected?
+    BOOST_TEST((std::is_same<decltype(ha.get_angle()), quantity<bud::plane_angle>>::value));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(equatorial_coord_constructors)
+
+BOOST_AUTO_TEST_CASE(equatorial_coord_default_right_ascension_constructor)
+{
+    equatorial_coord<double,
+            RightAscension<double, bu::quantity<bu::si::plane_angle>>,
+            quantity<bud::plane_angle>>
+            e;
+
+    //Create object of Right Ascension
+    RightAscension<double, bu::quantity<bu::si::plane_angle>>
+            ra(25.0 * bu::si::radian);
+
+    //Check value
+    BOOST_CHECK_CLOSE(ra.get_angle().value(), 25.0, 0.001);
+
+    //Quantity stored as expected?
+    BOOST_TEST((std::is_same<decltype(ra.get_angle()), quantity<bu::si::plane_angle>>::value));
+
+    //Check set_X_Declination
+    e.set_X_Declination(ra,39.0 * bud::degree);
+
+    BOOST_CHECK_CLOSE(e.get_X().get_angle().value(),25.0,0.001);
+    BOOST_CHECK_CLOSE(e.get_Declination().value(),39.0,0.001);
+
+    BOOST_TEST((std::is_same<decltype(e.get_X().get_angle()), quantity<bu::si::plane_angle>>::value));
+    BOOST_TEST((std::is_same<decltype(e.get_Declination()), quantity<bud::plane_angle>>::value));
+}
 
 BOOST_AUTO_TEST_SUITE_END()
